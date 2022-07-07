3f44896769524514bd28b66dc3ac7eb480de4c1e
Yanx <1832180488@qq.com>
2022-07-07 22:57:48 +0800

    branch

---
+++ src/com/test/branch/Branch.java
@@ -0,0 +1,11 @@
+package com.test.branch;
+
+/**
+ * @author Yanx
+ * @Create 2022-07-07-22:57
+ */
+public class Branch {
+    public static void main(String[] args) {
+        System.out.println("Branch");
+    }
+}
