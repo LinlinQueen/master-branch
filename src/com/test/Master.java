8f4403a5247badf482b446b0396d80fc92347da1
Yanx <1832180488@qq.com>
2022-07-07 22:48:23 +0800

    masterInit

---
+++ src/com/test/Master.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author Yanx
+ * @Create 2022-07-07-22:47
+ */
+public class Master{
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+}
